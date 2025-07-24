abcb2da30b33c4090839566eb64d252712fe10dd
Faholan <62927863+Faholan@users.noreply.github.com>
2025-07-23 17:46:48 -0700

    Better regex fixes

--- snip_gen/fix/fix_def.py
+++ snip_gen/fix/fix_def.py
@@ -8,27 +8,27 @@ REPLACEMENTS = [
         "",
     ),
     (  # Fiw bad UNITS definition
-        re.compile(r"UNITS\s*?(?:DATABASE)?\s*?(?:MICRONS)?\s*?(\d+)\s*?;?", re.IGNORECASE),
+        re.compile(r"^\s*UNITS\s*?(?:DATABASE)?\s*?(?:MICRONS)?\s*?(\d+)\s*?;?", re.MULTILINE),
         r"UNITS DISTANCE MICRONS \1;",
     ),
     (re.compile(r"END UNITS"), ""),
     (  # Fix bad block definitions missing a semicolon
         re.compile(
-            r"(COMPONENTS|FILLS|GROUPS|NETS|NONDEFAULTRULES|PINS|PINPROPERTIES|PROPERTYDEFINITIONS|REGIONS|SCANCHAINS|SPECIALNETS|VIAS)\s*?(\d)\s*?$",
+            r"^\s*(COMPONENTS|FILLS|GROUPS|NETS|NONDEFAULTRULES|PINS|PINPROPERTIES|PROPERTYDEFINITIONS|REGIONS|SCANCHAINS|SPECIALNETS|VIAS)\s*?(\d)\s*?$",
             re.MULTILINE,
         ),
         r"\1 \2 ;",
     ),
     (  # Fix bad VIA LAYER definitions
-        re.compile(r"(VIAS.*)LAYER (\w+) RECT(.*END VIAS)", re.DOTALL),
+        re.compile(r"^\s*(VIAS.*)LAYER (\w+) RECT(.*END VIAS)", re.DOTALL | re.MULTILINE),
         r"\1RECT \2\3",
     ),
     (  # Remove ANTENNA definitions
-        re.compile(r"\+\s*?ANTENNA\w*\d*\s*\w*\w*\s*"),
+        re.compile(r"^\s*\+\s*?ANTENNA\w+\s+[\d.]+\s+\w+\s+\w+", re.MULTILINE),
         "",
     ),
     (
-        re.compile(r"(PINS.*^\s*)(LAYER|PORT)(.*END PINS)", re.MULTILINE | re.DOTALL),
+        re.compile(r"^\s*(PINS.*^\s*)(LAYER|PORT)(.*END PINS)", re.DOTALL | re.MULTILINE),
         r"\1+ \2\3",  # Add a '+' before LAYER or PORT in PINS
     ),
     (  # Remove any MUSTJOIN net
@@ -36,20 +36,74 @@ REPLACEMENTS = [
         "",
     ),
     (
-        re.compile(r"(PINS.*)LAYER\s*(\w+)\s*RECT(.*END PINS)", re.DOTALL),
+        re.compile(r"^\s*(PINS.*)LAYER\s+(\w+)\s+RECT(.*END PINS)", re.DOTALL | re.MULTILINE),
         r"\1LAYER \2\3",  # Remove LAYER xxx RECT from PINS
     ),
     (
-        re.compile(r"(SPECIALNETS.*)WIDTH(.*END SPECIALNETS)", re.DOTALL),
+        re.compile(r"^\s*(SPECIALNETS.*)WIDTH(.*END SPECIALNETS)", re.DOTALL | re.MULTILINE),
         r"\1\2",  # Remove WIDTH from SPECIALNETS
     ),
     (
-        re.compile(r"(NETS.*)WIDTH(?:\s*\d+)?(.*END NETS)", re.DOTALL),
+        re.compile(r"^\s*(NETS.*)WIDTH(?:\s+\d+)?(.*END NETS)", re.DOTALL | re.MULTILINE),
         r"\1\2",  # Remove WIDTH from NETS
     ),
     (
-        re.compile(r"(COMPONENTS.*)(PLACED|FIXED|COVER)\s*(\d+)\s*(\d+)(\s*\w+.*END COMPONENTS)", re.DOTALL),
+        re.compile(
+            r"^\s*((COMPONENTS|PINS).*)(PLACED|FIXED|COVER)\s+(\d+)\s+(\d+)(\s+\w+.*END \2)", re.DOTALL | re.MULTILINE
+        ),
         r"\1\2 ( \3 \4 )\5",
+    ),
+    (
+        re.compile(r"^\s*ROW\s+(\w+)\s+(\w+)\s+\((\d+)\s+(\d+)\s*\)?", re.MULTILINE),
+        r"ROW \1 \3 \4 \5",
+    ),  # Fix issue where generated ROW have parentheses around coordinates
+    (
+        re.compile(r"^\s*(ROW\s+\w+\s+\w+\s+\d+\s+\d+\s+\w+\s+DO\s+\d+\s+BY\s+\d+\s+STEP\s+\d+\s+);", re.MULTILINE),
+        r"\1 0 ;",  # Add missing 0 Y coordinate in ROW definition
+    ),
+    (
+        re.compile(
+            r"^\s*(NETS.*)(COVER|FIXED|ROUTED|NOSHIELD|NEW)\s+(\w+)\s+\d+(.*END NETS)", re.DOTALL | re.MULTILINE
+        ),
+        r"\1\2 \3 \4",  # Remove incorrect width specification in NETS
+    ),
+    (
+        re.compile(
+            r'^\s*(PROPERTYDEFINITIONS.*)^(\s*\w+)\s+"(\w+)"?(.*END PROPERTYDEFINITIONS)', re.DOTALL | re.MULTILINE
+        ),
+        r"\1\2 \3\4",  # Remove quotes around property names in PROPERTYDEFINITIONS
+    ),
+    (
+        re.compile(r"\bR0\b"),
+        "N",
+    ),
+    (
+        re.compile(r"\bR180\b"),
+        "S",
+    ),
+    (
+        re.compile(r"\bR90\b"),
+        "W",
+    ),
+    (
+        re.compile(r"\bR180\b"),
+        "E",
+    ),
+    (
+        re.compile(r"\bMY\b"),
+        "FN",
+    ),
+    (
+        re.compile(r"\bMX\b"),
+        "FS",
+    ),
+    (
+        re.compile(r"\bMX90\b"),
+        "FW",
+    ),
+    (
+        re.compile(r"\bMY90\b"),
+        "FE",
     ),
 ]
 
